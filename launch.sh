331f7d549df07247146a16296952e80bdc407d06
Bence CZufor <czuforbence@gmail.com>
2020-10-08 22:49:55 +0200

    f

---
+++ launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+
+polybar  mybar
